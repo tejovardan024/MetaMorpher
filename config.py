1e6d830ed02d1a75888dfd5fc0b646c530ff4408
tejovardan024 <tejovardan047@gmail.com>
2026-04-07 11:22:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = os.environ.get("API_ID", "10811400")
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAHaimk-34KLyFKq9EBCW2cjshsr_j2wrIs")
-ADMIN = int(os.environ.get("ADMIN", '6469754522'))
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://HARSHA24:HARSHA24@cluster0.sxaj8up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+API_ID = os.environ.get("API_ID", "16995961")
+API_HASH = os.environ.get("API_HASH", "8817a7d4293049593e60999359970ddd")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8223534265:AAEGCECBgU83E0dQTGWFXimO4OidxqbRiIw")
+ADMIN = int(os.environ.get("ADMIN", '7364106679'))
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Anime_Encodes_Telugu:)
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "animeencodestelugusupport")
+DATABASE_URI = os.environ.get("DATABASE_URI","mongodb+srv://nothingboy024:nothingboy024@cluster0.5hzyn3i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 CAPTION = os.environ.get("CAPTION", "")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", ''7364106679))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8081"))
-LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
+LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002457918476)
